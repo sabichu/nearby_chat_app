2bfbc905f5a4f8076c9ec2afa815beb90bc63a82
Sabi Singh <sabisingh@MacBook-Air-de-Admin.local>
2024-10-27 11:06:26 +0100

    Modify HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,142 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final String userName = 'User${Random().nextInt(1000)}';
+  final Strategy strategy = Strategy.P2P_CLUSTER;
+  //final Map<String, ConnectionInfo> endpointMap = {};
+
+  final Map<String, String> discoveredEndpoints = {};
+  final Set<String> connectedEndpoints = {};
+
+  StreamSubscription? discoverySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _requestPermissions().then((_) {
+      _startAdvertising();
+      _startDiscovery();
+    });
+  }
+
+  @override
+  void dispose() {
+    Nearby().stopAdvertising();
+    Nearby().stopDiscovery();
+    Nearby().stopAllEndpoints();
+    discoverySubscription?.cancel();
+    super.dispose();
+  }
+
+  Future<void> _requestPermissions() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.bluetooth,
+      Permission.bluetoothConnect,
+      Permission.bluetoothScan,
+      Permission.location,
+      //Permission.nearbyWifiDevices,
+    ].request();
+
+    bool allGranted = statuses.values.every((status) => status.isGranted);
+
+    if (!allGranted) {
+      //_showSnackbar('No se concedieron todos los permisos necesarios.');
+    }
+  }
+
+  void _startAdvertising() async {
+    try {
+      bool advertising = await Nearby().startAdvertising(
+        userName,
+        strategy,
+        onConnectionInitiated: _onConnectionInit,
+        onConnectionResult: (id, status) {
+          if (status == Status.CONNECTED) {
+            setState(() {
+              connectedEndpoints.add(id);
+            });
+            //_showSnackbar('Conectado con $id');
+          } else {
+            //_showSnackbar('Error de conexión con $id: $status');
+          }
+        },
+        onDisconnected: (id) {
+          setState(() {
+            connectedEndpoints.remove(id);
+          });
+          //_showSnackbar('Desconectado de $id');
+        },
+      );
+      //_showSnackbar('Advertising iniciado: $advertising');
+    } catch (e) {
+      //_showSnackbar('Error al iniciar Advertising: $e');
+    }
+  }
+
+  void _startDiscovery() async {
+    try {
+      bool discovering = await Nearby().startDiscovery(
+        userName,
+        strategy,
+        onEndpointFound: (id, name, serviceId) {
+          setState(() {
+            discoveredEndpoints[id] = name;
+          });
+          _connectToDevice(id);
+        },
+        onEndpointLost: (id) {
+          setState(() {
+            discoveredEndpoints.remove(id);
+          });
+        },
+      );
+      //_showSnackbar('Discovery iniciado: $discovering');
+    } catch (e) {
+      //_showSnackbar('Error al iniciar Discovery: $e');
+    }
+  }
+
+  void _onConnectionInit(String id, ConnectionInfo info) {
+    Nearby().acceptConnection(
+      id,
+      onPayLoadRecieved: (endid, payload) {
+        if (payload.type == PayloadType.BYTES) {
+          String message = String.fromCharCodes(payload.bytes!);
+          //_showSnackbar('Mensaje de $endid: $message');
+        }
+      },
+      onPayloadTransferUpdate: (endid, payloadTransferUpdate) {
+        // Manejar actualizaciones de transferencia de payload si es necesario
+      },
+    );
+  }
+
+  void _connectToDevice(String id) {
+    Nearby().requestConnection(
+      userName,
+      id,
+      onConnectionInitiated: _onConnectionInit,
+      onConnectionResult: (id, status) {
+        if (status == Status.CONNECTED) {
+          setState(() {
+            connectedEndpoints.add(id);
+            discoveredEndpoints.remove(id);
+          });
+          //_showSnackbar('Conectado con $id');
+        } else {
+          //_showSnackbar('Error de conexión con $id: $status');
+        }
+      },
+      onDisconnected: (id) {
+        setState(() {
+          connectedEndpoints.remove(id);
+        });
+        //_showSnackbar('Desconectado de $id');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +173,8 @@ class _HomeScreenState extends State<HomeScreen> {
               Expanded(
                 child: ListView(
                   children: [
-
+                    UserCard(
+                        userName: 'Sabi Singh', deviceName: 'iPhone 13 Pro Max')
                   ],
                 ),
               ),
